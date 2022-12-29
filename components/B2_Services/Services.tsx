5f7ccffe28068d631f4888a80cc15992bb0358da
AlexAltrex <alexaltrex@mail.ru>
2022-12-29 07:34:44 +0300

    9

--- components/B2_Services/Services.tsx
+++ components/B2_Services/Services.tsx
@@ -3,7 +3,7 @@ import {IServiceCard, items} from "./items";
 import {Dot} from "../X_common/Dot/Dot";
 import {ButtonContained, ColorEnum} from "../X_common/ButtonContained/ButtonContained";
 import {AccordionItem} from "../X_common/AccordionItem/AccordionItem";
-import {FC, useState} from "react";
+import {FC, useEffect, useRef, useState} from "react";
 
 const ServiceCard: FC<IServiceCard> = ({
                                            title,
@@ -69,12 +69,73 @@ const ServiceCard: FC<IServiceCard> = ({
 
 //========= Services =========//
 export const Services = () => {
+    const [delta1, setDelta1] = useState(0);
+    const [delta2, setDelta2] = useState(0);
+    const [delta3, setDelta3] = useState(0);
+    const koef = 0.3;
+    const deltas = [delta1, delta2, delta3]
+
+    useEffect(() => {
+        const onScroll = () => {
+            if (ref1 && ref1.current) {
+                const rect1 = ref1.current.getBoundingClientRect();
+                console.log(rect1.top)
+                console.log(window.innerHeight)
+                console.log("---")
+                if (rect1.top < window.innerHeight && rect1.top > 0) {
+                    setDelta1(koef * (window.innerHeight - rect1.top))
+                }
+                if (rect1.top > window.innerHeight) {
+                    setDelta1(0)
+                }
+            }
+            if (ref2 && ref2.current) {
+                const rect2 = ref2.current.getBoundingClientRect();
+                //console.log(window.innerHeight - rect2.top)
+                if (rect2.top < window.innerHeight && rect2.top > 0) {
+                    setDelta2(koef * (window.innerHeight - rect2.top))
+                }
+                if (rect2.top > window.innerHeight) {
+                    setDelta2(0)
+                }
+            }
+            if (ref3 && ref3.current) {
+                const rect3 = ref3.current.getBoundingClientRect();
+                //console.log(window.innerHeight - rect3.top)
+                if (rect3.top < window.innerHeight && rect3.top > 0) {
+                    setDelta3(koef * (window.innerHeight - rect3.top))
+                }
+                if (rect3.top > window.innerHeight) {
+                    setDelta3(0)
+                }
+            }
+
+        };
+        window.addEventListener("scroll", onScroll, {passive: true})
+    }, [])
+
+    const ref1 = useRef<HTMLDivElement>(null!);
+    const ref2 = useRef<HTMLDivElement>(null!);
+    const ref3 = useRef<HTMLDivElement>(null!);
+    const refs = [ref1, ref2, ref3];
+
     return (
         <div className={style.services}>
-            <div className={style.inner}>
+            <div className={style.inner}
+
+            >
                 {
                     items.map((card, key) => (
-                        <ServiceCard key={key} {...card}/>
+                        <div ref={refs[key]}
+                             style={{
+                                 marginTop: `-${deltas[key]}px`,
+                                 position: "relative",
+                                 zIndex: 10 * key,
+                             }}
+                        >
+                            <ServiceCard key={key} {...card}/>
+                        </div>
+
                     ))
                 }
             </div>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,21 @@
 import "../assets/scss/global.scss";
-import type { AppProps } from 'next/app'
+import type {AppProps} from 'next/app'
 import {store, Store} from "../store/store";
-import {createContext, useEffect} from "react";
+import {createContext, useEffect, useLayoutEffect, useState} from "react";
 import "aos/dist/aos.css";
 import AOS from "aos";
 
-export const StoreContext = createContext<Store>({} as Store)
+import {gsap} from "gsap-trial";
+import {ScrollTrigger} from "gsap-trial/dist/ScrollTrigger";
+import {ScrollSmoother} from "gsap-trial/dist/ScrollSmoother";
 
-export default function App({ Component, pageProps }: AppProps) {
+
+export const StoreContext = createContext<Store>({} as Store);
+const SmootherContext = createContext(null as ScrollSmoother | null);
+
+const useIsomorphicLayoutEffect = typeof window !== "undefined" ? useLayoutEffect : useEffect;
+
+export default function App({Component, pageProps}: AppProps) {
     useEffect(() => {
         AOS.init({
             easing: "ease-out-cubic",
@@ -15,10 +23,26 @@ export default function App({ Component, pageProps }: AppProps) {
         });
     }, []);
 
-    return (
-      <StoreContext.Provider value={store}>
-        <Component {...pageProps} />
-      </StoreContext.Provider>
+    // const [smoother, setSmoother] = useState<ScrollSmoother | null>(null);
+    // useIsomorphicLayoutEffect(() => {
+    //     gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+    //
+    //     let smoother = ScrollSmoother.create({
+    //         //smooth: 1,
+    //         normalizeScroll: true, // prevents address bar from showing/hiding on most devices, solves various other browser inconsistencies
+    //         //ignoreMobileResize: true, // skips ScrollTrigger.refresh() on mobile resizes from address bar showing/hiding
+    //         //effects: true,
+    //         //preventDefault: true
+    //     });
+    //
+    //     setSmoother(smoother);
+    // }, []);
 
-      )
+    return (
+        //     <SmootherContext.Provider value={smoother}>
+        <StoreContext.Provider value={store}>
+            <Component {...pageProps} />
+        </StoreContext.Provider>
+        // </SmootherContext.Provider>
+    )
 }
