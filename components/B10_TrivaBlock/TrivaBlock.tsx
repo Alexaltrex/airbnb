488a8111bf46a832b1920ccf83e88ef99f9b0eee
AlexAltrex <alexaltrex@mail.ru>
2023-08-28 06:17:39 +0300

    20

--- components/B10_TrivaBlock/TrivaBlock.tsx
+++ components/B10_TrivaBlock/TrivaBlock.tsx
@@ -84,6 +84,7 @@ export const TrivaBlock = () => {
                                muted={true}
                                playsInline={true}
                                loop={false}
+                               controls={true}
                         />
                     </div>
 
